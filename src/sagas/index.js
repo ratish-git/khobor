1850491aaa31a3d43c163669f46a67837d86e609
Ratish Barman <ratish.barman4@gmail.com>
2020-10-10 23:15:53 +0530

    url

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,7 @@
 import { put, takeLatest, all } from "redux-saga/effects";
 function* fetchFood() {
   const json = yield fetch(
-    "http://newsapi.org/v2/top-headlines?country=in&apiKey=ce3c8c93755246e5a8cef5b3e16566ac"
+    "https://newsapi.org/v2/top-headlines?country=in&apiKey=ce3c8c93755246e5a8cef5b3e16566ac"
   ).then((response) => response.json());
   yield put({ type: "FOOD_RECEIPE", json: json.articles });
 }
